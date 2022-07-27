d5e900b231df9631a58bddb6541c67d5a6a4c8e4
dayren86 <90610568+dayren86@users.noreply.github.com>
2022-07-27 11:20:02 +0300

    on delete cascade developers projects

--- src/main/resources/db/migration/V5__on_delete_cascade.sql
+++ src/main/resources/db/migration/V5__on_delete_cascade.sql
@@ -4,6 +4,9 @@ DROP CONSTRAINT developers_skills_developers_id_fkey;
 ALTER TABLE developers_projects
 DROP CONSTRAINT developers_projects_developers_id_fkey;
 
+ALTER TABLE developers_projects
+DROP CONSTRAINT developers_projects_projects_id_fkey;
+
 
 ALTER TABLE IF EXISTS public.developers_skills
     ADD CONSTRAINT developers_skills_developers_id_fkey FOREIGN KEY (developers_id)
@@ -13,4 +16,9 @@ ALTER TABLE IF EXISTS public.developers_skills
 ALTER TABLE IF EXISTS public.developers_projects
     ADD CONSTRAINT developers_projects_developers_id_fkey FOREIGN KEY (developers_id)
     REFERENCES public.developers (id)
+	ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS public.developers_projects
+    ADD CONSTRAINT developers_projects_projects_id_fkey FOREIGN KEY (projects_id)
+    REFERENCES public.projects (id)
 	ON DELETE CASCADE;
